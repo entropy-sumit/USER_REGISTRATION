cc3ad6e6105939d5923b7da1442d0342c329a53b
entropy-sumit <sumitr600@gmail.com>
2022-01-09 23:34:52 +0530

    [Add] sumit added Uc1 program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //UC1
+
+            UC1FirstName firstName = new UC1FirstName(); ;
+            Console.WriteLine("Please Enter your first name");
+            var check = firstName.ValidateFirstName(Console.ReadLine());
+            Console.WriteLine(check);
         }
     }
 }
---
+++ UC1FirstName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class UC1FirstName
+    {
+        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+
+        public bool ValidateFirstName(string fName)
+        {
+            return Regex.IsMatch(fName, FIRST_NAME_REGEX);
+        }
+    }
+}
