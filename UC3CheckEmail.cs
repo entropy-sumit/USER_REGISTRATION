5483c15fa74919879cdc91037d40da68238986a1
entropy-sumit <sumitr600@gmail.com>
2022-01-09 23:56:22 +0530

    [Add] sumit added UC3 program

---
+++ UC3CheckEmail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+   public class UC3CheckEmail
+    {
+        public static string REGEX_EMail = "^(abc).?[a-z]{3,}[@](bl).?(co).?[a-z]{2,}$"; //abc.xyz@bl.co.in
+
+        public bool ValidateEMail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMail);
+        }
+    }
+}
