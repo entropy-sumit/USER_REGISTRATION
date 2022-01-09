50b3263201b47bfdd36ec8fc7012becc446505cb
entropy-sumit <sumitr600@gmail.com>
2022-01-09 23:48:59 +0530

    [Add] sumit added Uc2 program

--- Program.cs
+++ Program.cs
@@ -6,12 +6,20 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            //UC1
+            static void Main(string[] args)
+            {
+                //UC1
+                UC1FirstName firstName = new UC1FirstName(); ;
+                Console.WriteLine("Please Enter your first name");
+                var check = firstName.ValidateFirstName(Console.ReadLine());
+                Console.WriteLine(check);
 
-            UC1FirstName firstName = new UC1FirstName(); ;
-            Console.WriteLine("Please Enter your first name");
-            var check = firstName.ValidateFirstName(Console.ReadLine());
-            Console.WriteLine(check);
+                //UC2
+                UC2LastName lastName = new UC2LastName();
+                Console.WriteLine("Please Enter your last name");
+                var check2 = lastName.ValidateFirstName(Console.ReadLine());
+                Console.WriteLine(check2);
+            }
         }
     }
 }
---
+++ UC2LastName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class UC2LastName
+    {
+        public static string LAST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+
+        public bool ValidateFirstName(string lName)
+        {
+            return Regex.IsMatch(lName, LAST_NAME_REGEX);
+        }
+    }
+}
