2bffcd4aa6bad1b74d0136ec0b540ac482b59563
entropy-sumit <sumitr600@gmail.com>
2022-01-10 17:42:54 +0530

    [Add] sumit added uc5 program

---
+++ UC5Password.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UC5Password
+    {
+        public static string REGEX_Password1 = "^[A-Z a-z]{8,}$"; //shubhamsth, ShubhamSth
+
+        public bool ValidatePassword1(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password1);
+        }
+    }
+}
